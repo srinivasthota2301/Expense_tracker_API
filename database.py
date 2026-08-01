0475efd396d092576b9bd5449d9234cfccdef320
Srinivas Thota <srinivasthota2301@gmail.com>
2026-08-01 21:05:35 +0530

    Refactor DATABASE_URL for better readability

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY@mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL = (
+    "mysql+pymysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY"
+    "mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb"
+)
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"ssl": {}}
+    connect_args={
+        "ssl": {}
+    }
 )
 
 SessionLocal = sessionmaker(
@@ -15,6 +20,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
