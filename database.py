e50b03954a55075b006debf90c8074940c744ec6
Srinivas Thota <srinivasthota2301@gmail.com>
2026-08-01 21:23:31 +0530

    Fix database connection URL formatting

--- database.py
+++ database.py
@@ -2,15 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = (
-    "mysql+pymysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY"
+    "mysql+pymysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY@"
     "mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb"
 )
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "ssl": {}
-    }
+    connect_args={"ssl": {}}
 )
 
 SessionLocal = sessionmaker(
@@ -20,7 +18,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
