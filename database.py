0caacddab4132a4021c8a1c83f0fde6918eb8479
Srinivas Thota <srinivasthota2301@gmail.com>
2026-08-01 20:47:50 +0530

    Change database URL to use pymysql driver

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY@mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY@mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb?ssl-mode=REQUIRED"
 
 engine = create_engine(
     DATABASE_URL,
