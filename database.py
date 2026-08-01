ce50084df56d8c1e32ca199508e773928c01eaef
Srinivas Thota <srinivasthota2301@gmail.com>
2026-08-01 17:12:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/expense"
+DATABASE_URL = "mysql://avnadmin:AVNS__4H6kfF2w-YkG0mi2bY@mysql-4e56d22-srinivasthota2301-e69a.c.aivencloud.com:22292/defaultdb?ssl-mode=REQUIRED"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {}}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
